33c621a724397d4492a79f5c1cd47a4d6f62a984
nimadorostkar <nimadorostkar97@gmail.com>
2021-03-08 16:19:52 +0330

    push changes

--- shelter/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class QuickContactForm(forms.Form):
-    email= forms.CharField(label='',widget= forms.EmailInput(attrs={'placeholder':'Enter your E-mail address'}))
-    textarea = forms.CharField(label='',widget=forms.Textarea(attrs={'placeholder': 'Write here'}))
