bf868e49bb4703e4081d8e7e9fd5e3ae23fc9af9
nima <54316106+nimadorostkar@users.noreply.github.com>
2021-09-19 15:25:47 +0430

    Update models.py

--- core/models.py
+++ core/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+
+
+
 # ================================================================== >> COUNTRY
 class Country(models.Model):
     """Country Model."""
